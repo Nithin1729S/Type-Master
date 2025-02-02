ee299ddab2ad94c9cecf1cdeda720eac1212b4c1
Nithin1729S <sureshnithin1729@gmail.com>
2025-02-03 03:42:52 +0530

    Typing Area fi

---
+++ components/MainContent.tsx
@@ -0,0 +1,158 @@
+"use client";
+
+import { TypingArea } from "./TypingArea";
+import { useCallback, useEffect, useState } from "react";
+import { wordsList } from '../utils/wordsList.js'
+
+const getRandomWords = () => {
+    let words = wordsList.split(" ");
+    let randomWords = [];
+    for (let i = 0; i < 100; i++) {
+        randomWords.push(words[Math.floor(Math.random() * words.length)]);
+    }
+    let joinedWords = randomWords.join(" ");
+    return joinedWords.replace("  ", " ");
+}
+
+export function MainContent() {
+
+    const totalTime = 30;
+
+    const [done, setDone] = useState(false);
+    const [timer, setTimer] = useState(0);
+    const [words, setWords] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (!done) {
+                setTimer(prevTimer => {
+                    if (prevTimer === totalTime) {
+                        setDone(true);
+                        return 0;
+                    }
+                    return prevTimer + 1;
+                });
+            }
+        }, 1000);
+        return () => clearInterval(interval);
+    }, [done]);
+
+    const resetBox = () => {
+        setTimer(0);
+        setDone(!done);
+        setTextToType(getRandomWords());
+        setText(textToType.split("").map((letter) => ({ letter: letter, entered: "false" })));
+    }
+
+    const keyDownHandler = useCallback((event: KeyboardEvent) => {
+        if (event.code === "Tab") {
+            resetBox();
+            event.preventDefault();
+            return;
+        }
+    }, [resetBox]);
+
+    useEffect(() => {
+        document.addEventListener("keydown", keyDownHandler);
+        return () => document.removeEventListener("keydown", keyDownHandler);
+    }, [keyDownHandler]);
+
+    const calculateRemainingTime = (currentTime: number) => {
+        return (totalTime - currentTime);
+    }
+
+    const [textToType, setTextToType] = useState("");
+
+    useEffect(() => {
+        const randomWords = getRandomWords();
+        setTextToType(randomWords);
+    }, []);
+
+    let [text, setText] = useState<{ letter: string; entered: string }[]>([]);
+
+    useEffect(() => {
+        setText(textToType.split("").map((letter) => ({ letter: letter, entered: "false" })));
+    }, [textToType]);
+
+
+    const calculateWPM = () => {
+        let words = 0;
+        let correct = true;
+        for (let letter in text) {
+            if (text[letter].entered === "wrong") {
+                correct = false;
+                continue;
+            }
+            if (text[letter].entered === "false") {
+                correct = false;
+                continue;
+            }
+            if (text[letter].letter === " ") {
+                if (correct) words++;
+                correct = true;
+                continue;
+            }
+        }
+        return Math.ceil((words * 60) / totalTime);
+    }
+
+    const calculateRawWPM = () => {
+        let words = 0;
+        let correct = true;
+        for (let letter in text) {
+            if (text[letter].entered === "false") {
+                correct = false;
+                continue;
+            }
+            if (text[letter].letter === " ") {
+                if (correct) words++;
+                correct = true;
+                continue;
+            }
+        }
+        return Math.ceil((words * 60) / totalTime);
+    }
+
+    const calculateAccuracy = () => {
+        let correct = 0;
+        let wrong = 0;
+        for (let letter in text) {
+            if (text[letter].entered === "wrong") wrong++;
+            if (text[letter].entered === "correct") correct++;
+        }
+        let accuracy = ((correct / (correct + wrong)) * 100).toFixed(2);
+        if (isNaN(Number(accuracy))) return 0;
+        return accuracy;
+    }
+
+    return (
+        <div className="row-span-1 col-span-6 grid grid-rows-[0.1fr_3fr_1fr] sm:grid-rows-[0.5fr_4fr_1fr] mt-2 mr-4 ml-4 sm:mt-10 sm:mr-20 sm:ml-20">
+            <div id="settings" className="row-span-1">
+
+            </div>
+            {done ?
+                <div id="stats-display">
+                    <div id="wpm-display" className="mt-20 font-black text-6xl sm:text-8xl text-center text-gray-100 ">WPM: {calculateWPM()}</div>
+                    <div className="font-semibold text-xl sm:text-2xl text-center mt-5 mb-8 text-gray-400">Raw WPM: {calculateRawWPM()}, Accuracy: {calculateAccuracy() === "NaN" ? 0 : calculateAccuracy()}%</div>
+                    <p className="text-center pb-4 sm:pb-0">WPM indicates the number of correct words typed per minute.</p>
+                    <p className="text-center pb-4 sm:pb-0"> Raw WPM indicates the total number of words typed per minute, correct or not.</p>
+                    <p className="text-center"> Accuracy is the percentage of characters correctly entered.</p>
+                </div> :
+                <div id="main-container">
+                    <div id="timer">
+                        <p className="text-base sm:text-xl text-left mb-6 sm:mb-0 sm:pl-20 text-gray-400 font-bold">Remaining Time: {calculateRemainingTime(timer)}</p>
+                    </div>
+                    <div id="main-box-container">
+                        <input id="fake-input-box" type="text" className="caret-transparent text-gray-900 bg-gray-900 hover:border-0 hover:bg-gray-900 focus:outline-none focus:border-0" spellCheck={false} autoFocus></input>
+                        <TypingArea setTimer={setTimer} text={text} setText={setText} textToType={textToType} setTextToType={setTextToType} getRandomWords={getRandomWords} />
+                    </div>
+                </div>
+            }
+            <div id="instructions" className="row-span-1 flex flex-col mt-6 sm:mt-0 sm:block">
+                <button className="p-3 bg-gray-800 sm:hidden" onClick={resetBox}>Restart Test</button>
+                <p className="text-center hidden sm:block">Press <span className="text-gray-200">TAB</span> to restart test.</p>
+                
+            </div>
+        </div>
+    );
+}
---
+++ components/TypingArea.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { useCallback, useEffect, useState } from 'react'
+
+const isLetter = (str: string) => {
+    if (str === "Space") return " ";
+    if (str.length != 4) return false;
+    if (str.substring(0, 3) === "Key") return str.substring(3, 4);
+    return false;
+}
+
+interface TypingAreaProps {
+    setTimer: React.Dispatch<React.SetStateAction<number>>;
+    text: { letter: string; entered: string }[];
+    setText: React.Dispatch<React.SetStateAction<{ letter: string; entered: string }[]>>;
+    textToType: string;
+    setTextToType: React.Dispatch<React.SetStateAction<string>>;
+    getRandomWords: () => string;
+}
+
+export function TypingArea({ setTimer, text, setText, textToType, setTextToType, getRandomWords }: TypingAreaProps) {
+    const [currentLetter, setCurrentLetter] = useState(0);
+
+    const resetBox = () => {
+        const randomWords = getRandomWords();
+        setTextToType(randomWords);
+        setText(textToType.split("").map((letter) => ({ letter: letter, entered: "false" })));
+        setCurrentLetter(0);
+        setTimer(0);
+    }
+
+    interface TextData {
+        letter: string;
+        entered: string;
+    }
+
+    const addLetter = (letter: string) => {
+        letter = letter.toLowerCase();
+        let correct = textToType[currentLetter] === letter;
+        setText((text: TextData[]) => text.map((data, index) => {
+            if (index !== currentLetter) return data;
+            return { letter: data.letter, entered: correct ? "correct" : "wrong" };
+        }));
+        setCurrentLetter(currentLetter + 1);
+    }
+
+    const deleteLetter = () => {
+        if (currentLetter === 0) return;
+        setText(text => text.map((data, index) => {
+            if (index !== currentLetter - 1) return data;
+            return { letter: data.letter, entered: "false" };
+        }));
+        setCurrentLetter(currentLetter - 1);
+    }
+
+    const keyDownHandler = useCallback((event: KeyboardEvent) => {
+        console.log(event.code);
+        if (event.code === "Tab") {
+            resetBox();
+            event.preventDefault();
+            return;
+        }
+        if (event.code === "Backspace") {
+            deleteLetter();
+            return;
+        }
+        if (event.code === "Space") {
+            addLetter(" ");
+            event.preventDefault();
+            return;
+        }
+        let letter = isLetter(event.code);
+        if (letter === false) return;
+        addLetter(letter);
+    }, []);
+
+    useEffect(() => {
+        document.addEventListener("keydown", keyDownHandler);
+        return () => document.removeEventListener("keydown", keyDownHandler);
+    }, [keyDownHandler]);
+
+    return (
+        <div id="typing-area" className="w-full h-full bg-inherit text-lg sm:text-3xl sm:pl-20 sm:pr-20">
+            {text.map((data, i) => {
+                return <span key={i} className={(i === currentLetter ? "animate-blink border-l-2" : "border-l-2 border-gray-900") + " " + (data.letter === " " ? "ml-[0.3rem]" : "ml-[0.05rem]") + " box-border " + (data.entered === "false" ? "text-gray-700" : (data.entered === "wrong" ? "text-red-900" : "text-gray-300"))}>{data.letter}</span>
+            })}
+        </div>
+    );
+}
